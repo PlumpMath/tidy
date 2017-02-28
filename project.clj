0c89fa56809327ff346337e65a9ca72e5a5780f3
Tom Goldsmith <goldsmith.tee@gmail.com>
2017-02-28 16:02:26 -0500

    qualify name>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tidy "0.1.1"
+(defproject tomthought/tidy "0.1.2"
   :description "Concurrency patterns using Clojure core.async."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
